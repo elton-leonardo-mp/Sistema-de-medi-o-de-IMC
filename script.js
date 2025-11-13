45d7530cbbc428c48ce2e08caeca4d00b4fa3d01
elton-leonardo-mp <eltonleo32118@gmail.com>
2025-11-13 01:24:11 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const calcularButton = document.getElementById("calcular");
+const resultadoElement = document.getElementById("resultado");
+
+calcularButton.addEventListener("click", calcularIMC);
+
+function calcularIMC() {
+  const altura = parseFloat(document.getElementById("altura").value);
+  const peso = parseFloat(document.getElementById("peso").value);
+  const imc = peso / (altura * altura);
+  let classificacao;
+
+  if (imc < 18.5) {
+    classificacao = "Magreza";
+  } else if (imc < 25) {
+    classificacao = "Peso normal";
+  } else if (imc < 30) {
+    classificacao = "Sobrepeso";
+  } else if (imc < 35) {
+    classificacao = "Obesidade tipo 1";
+  } else if (imc < 40) {
+    classificacao = "Obesidade tipo 2";
+  } else if (imc > 40) {
+    classificacao = "Obesidade tipo 3";
+  }
+
+  resultadoElement.textContent = `Seu IMC é ${imc.toFixed(
+    2
+  )} (${classificacao})`;
+}
+document.getElementById("limpar").addEventListener("click", function () {
+  document.getElementById("altura").value = "";
+  document.getElementById("peso").value = "";
+  document.getElementById("resultado").textContent = "";
+});
